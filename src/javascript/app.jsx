ffbbc794c7ad7d72f6a9182c78748cedf06b0fe7
Leon Wright <techman83@gmail.com>
2015-09-03 21:06:51 +0800

    Use S3 url

--- src/javascript/app.jsx
+++ src/javascript/app.jsx
@@ -7,7 +7,7 @@ document.body.innerHTML += '<div id="content"></div>';
 
 React.render(
   <NetKANs
-    url="https://dl.dropboxusercontent.com/u/8415802/status/netkan.json"
+    url="http://status.ksp-ckan.org/status/netkan.json"
     pollInterval={300000} />,
   document.getElementById('content')
 );
