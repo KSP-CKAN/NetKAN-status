4eeb6b21e9fc2e79cd16656727591a7a4f8c7fca
Paul Hebble <pjhebble@gmail.com>
2017-09-09 17:50:29 -0500

    UI tweaks to ease error investigation
    Top row:
    
    - Slightly darken background
    - Add favicon in upper left
    - Make title smaller and lighter
    - Move search box to upper right
    - Make search box wider
    - Auto focus search box
    
    Grid:
    
    - Shrink text
    - Fill whole space
    - Filter by both NetKAN and last error
    - Sort errors to top by default
    - Use triangles instead of arrows for sort
    - Shrink headers and rows slightly
    - Make timestamp columns static width
    - Make error column grow to fit whole errors
    
    Framework:
    
    - Support newer Nodejs
    - Use newest dependencies

--- src/javascript/app.jsx
+++ src/javascript/app.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import NetKANs from './components/NetKANs.jsx';
 import datatableStyles from
-  '../../node_modules/fixed-data-table/dist/fixed-data-table.min.css'
+  '../../node_modules/fixed-data-table/dist/fixed-data-table.min.css';
+
+var sheet = document.createElement('style');
+sheet.type = 'text/css';
+sheet.innerHTML = 'html,body{background-color:#f0f0f0;} input::-webkit-input-placeholder{font-style:italic;}';
+document.body.appendChild(sheet);
 
 document.body.innerHTML += '<div id="content"></div>';
 
-React.render(
+ReactDom.render(
   <NetKANs
     url="http://status.ksp-ckan.org/status/netkan.json"
     pollInterval={300000} />,
